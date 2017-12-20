8947a97925aeb33cfae071d72114b112d7815978
Johan Roxendal <johan.roxendal@gu.se>
2017-12-20 15:26:14 +0100

    testing other wait value for travis

--- e2e/tests.js
+++ e2e/tests.js
@@ -20,7 +20,7 @@ test.page(`${host}/listor/sprak/original`)(
         await t
             .click(Selector(".filters select").nth(0))
             .click(Selector("option[value='till']"))
-            .wait(100)
+            .wait(200)
             .expect(getLocation()).eql(`${host}/listor/sprak/till/`)
 
         let initialLis = await Selector("li").count
