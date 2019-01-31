8faa89af12244bf38249d9c62ef7d932a90e5fe5
Johan Roxendal <johan.roxendal@gu.se>
2019-01-31 11:06:51 +0100

    added basehref to ga log

--- plugins/ga.js
+++ plugins/ga.js
@@ -3,8 +3,7 @@
 export default ({ app }) => {
   app.router.afterEach((to, from) => {
     if(typeof window != 'undefined' && window.gtag) {
-      console.log("pageview", to.fullPath)
-      window.gtag('config', 'UA-132486790-2', { page_path: to.fullPath });
+      window.gtag('config', 'UA-132486790-2', { page_path: "/översättarlexikon/" + to.fullPath });
     }
   })
 }
