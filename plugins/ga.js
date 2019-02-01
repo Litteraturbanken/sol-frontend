c096e5653263b94cc95a2fe61777a8d082097f12
Johan Roxendal <johan.roxendal@gu.se>
2019-02-01 12:17:27 +0100

    fixed path issue with ga logging

--- plugins/ga.js
+++ plugins/ga.js
@@ -3,7 +3,7 @@
 export default ({ app }) => {
   app.router.afterEach((to, from) => {
     if(typeof window != 'undefined' && window.gtag) {
-      window.gtag('config', 'UA-132486790-2', { page_path: "/översättarlexikon/" + to.fullPath });
+      window.gtag('config', 'UA-132486790-2', { page_path: "/översättarlexikon" + to.fullPath });
     }
   })
 }
