74828cbe602524f64da7e4e072cbf5dd6ae56b05
Johan Roxendal <johan.roxendal@litteraturbanken.se>
2023-05-22 14:17:07 +0200

    fixed issue with sorting weird letters

--- assets/utils.js
+++ assets/utils.js
@@ -1,69 +1,72 @@
-import nSorter from "natural-sort"
-import * as _ from "lodash"
+import nSorter from "natural-sort";
+import * as _ from "lodash";
 
 export function debounce(func, wait, immediate) {
-    var timeout
-    return function() {
+  var timeout;
+  return function() {
+    let context = this;
+    let args = arguments;
 
-        let context = this
-        let args = arguments
+    return new Promise(function(resolve) {
+      var later = function() {
+        timeout = null;
+        if (!immediate) resolve(func.apply(context, args));
+      };
 
-        return new Promise(function(resolve) {
-            var later = function() {
-                timeout = null
-                if (!immediate) resolve(func.apply(context, args))
-            }
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
 
-            var callNow = immediate && !timeout
-            clearTimeout(timeout)
-            timeout = setTimeout(later, wait)
-
-            if (callNow) resolve(func.apply(context, args))
-        })
-    }
+      if (callNow) resolve(func.apply(context, args));
+    });
+  };
 }
 
 export function naturalSort(array, ...sortKeys) {
-    // sorts array in place
-    let sorter = nSorter()
-    let transposer = (char) => {
-        let trans = _.fromPairs(
-            _.zip(
-                "ÁÂÃÄÅÇČĆÈÉÊËĖÌÍÎÏÑÒÓÔÕØŌÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõøōùúûüýÿŠščāĐđ".split(""),
-                "AAAÅÄCCCEEEEEIIIINOOOOÖOUUUUYaaaaåäceeeeiiiinooooöouuuuyySscaDd".split("")
-            )
+  // sorts array in place
+  let sorter = nSorter();
+  let transposer = char => {
+    let trans = _.fromPairs(
+      _.zip(
+        "ÁÂÃÄÅÇČĆÈÉÊËĖÌÍÎÏÑÒÓÔÕØŌÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõøōùúûüýÿŠščāĐđěřŁŚ".split(
+          ""
+        ),
+        "AAAÅÄCCCEEEEEIIIINOOOOÖOUUUUYaaaaåäceeeeiiiinooooöouuuuyySscaDderLS".split(
+          ""
         )
-        trans = _.extend(
-            trans,
-            _.fromPairs(
-                _.zip(
-                    ["Æ", "æ", "Ð", "ð", "Þ", "þ", "ß", "Œ", "œ"],
-                    ["AE", "ae", "DH", "dh", "TH", "th", "ss", "OE", "oe"]
-                )
-            )
+      )
+    );
+    trans = _.extend(
+      trans,
+      _.fromPairs(
+        _.zip(
+          ["Æ", "æ", "Ð", "ð", "Þ", "þ", "ß", "Œ", "œ"],
+          ["AE", "ae", "DH", "dh", "TH", "th", "ss", "OE", "oe"]
         )
+      )
+    );
 
-        return trans[char] || char
+    return trans[char] || char;
+  };
+  function stripChars(str) {
+    return str.replace(/[ʿ"'\-\–\.…]/g, "").replace(/^ */, "");
+  }
+  function getSortKey(obj) {
+    let output = null;
+    for (let key of sortKeys) {
+      output = output || obj[key];
     }
-    function stripChars(str) {
-        return str.replace(/["'\-\–\.…]/g, "").replace(/^ */, "")
+    return output;
+  }
+  array.sort((a, b) => {
+    a = getSortKey(a);
+    b = getSortKey(b);
+    if (typeof a == "string") {
+      a = _.map(stripChars(a), transposer).join("");
     }
-    function getSortKey(obj) {
-        let output = null
-        for(let key of sortKeys) {
-            output = output || obj[key]
-        }
-        return output
+    if (typeof b == "string") {
+      b = _.map(stripChars(b), transposer).join("");
     }
-    array.sort((a, b) => {
-        a = getSortKey(a)
-        b = getSortKey(b)
-        if(typeof a == "string") {
-            a = _.map(stripChars(a), transposer).join("")
-        }
-        if(typeof b == "string") {
-            b = _.map(stripChars(b), transposer).join("")
-        }
-        return sorter(a, b)
-    })
+    return sorter(a, b);
+  });
 }
